f6b0b50e1705051afecbdc629b1fffad8630cfb0
Dharmesh Jogadia <dharmeshjogadia@gmail.com>
2014-03-13 20:02:58 +0530

    AC

--- Little Chef and Numbers.cpp
+++ Little Chef and Numbers.cpp
@@ -7,19 +7,23 @@ Problem code: PROSUM
 int main()
 {
 	int t;
-	long long n,cnt,tmp;
+	long long n,cnt,cnt2,tmp;
 	scanf("%d",&t);
 	while(t--)
 	{
 		scanf("%lld",&n);	
 		cnt=0;
+		cnt2=0;
 		while(n--)
 		{
 			scanf("%lld",&tmp);
-			if(tmp !=1 && tmp!=0)
-				cnt++;
+		
+				if(tmp>=2)
+					cnt++;
+				if(tmp==2)
+					cnt2++;
 		}
-		printf("%lld\n",cnt*(cnt-1)/2);
+		printf("%lld\n",((cnt*(cnt-1)/2))-(cnt2*(cnt2-1)/2));
 	}
 	return 0;
 }
